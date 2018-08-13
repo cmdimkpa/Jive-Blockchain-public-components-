13f8a943f0d43acfb13fb05bfaabc3cfd3aad0dc
Monty Dimkpa <cmdimkpa@gmail.com>
2018-08-13 05:32:41 +0100

    Update jive-miner.py

--- jive-miner.py
+++ jive-miner.py
@@ -13,6 +13,7 @@
 #  * pip2 install rigo                                       #
 #  * pip2 install rrds_template                              #
 #  * pip2 install jive_pow                                   #
+#  * pip2 install jive_session                               #
 #  * pip2 install requests                                   #
 #                                                            #
 #  Copyright (c) Monty Dimkpa. All Rights Reserved.          #
@@ -20,23 +21,25 @@
 
 from rrds_template.lib import *
 from jive_pow.POW import *
+from jive_session.session import Session
 import requests as http
 
 global miner_ip, next_coords, hello_world, submit_pow
 
 miner_ip = "127.0.0.1"  # change this to your Server IP
-next_coords = "http://router.jive-interface.net/miner/next-coords"
+next_coords = "http://router.jive-interface.net/miner/next-coords/"
 submit_pow = "http://router.jive-interface.net/miner/submit-pow"
-hello_world = "http://router.jive-interface.net/miner/hello-world"
+hello_world = "http://router.jive-interface.net/miner/hello-world/"
 
 @app.route("/mine")
 def mine():
     try:
-        feedback = http.get(next_coords).content
+        session = Session(request)
+        feedback = http.get(next_coords+session[1]).content
         if "Error: solution limit reached" in feedback:
             return feedback
         start,stop = eval(eval(feedback))
-        data = {"pow":proof_of_work(start,stop)}
+        data = {"pow":proof_of_work(start,stop),"session":session}
         return http.post(submit_pow,json.dumps(data)).content
     except Exception as e:
         return str(e)
@@ -44,7 +47,8 @@ def mine():
 @app.route("/hello-world") # run this after setting up for the first time
 def hello_world_():
     try:
-        return http.get(hello_world).content
+        session = Session(request)
+        return http.get(hello_world+session[0]).content
     except Exception as e:
         return str(e)
 
